2cf1d47a9ae392fb64a1169bf4f95f9a9e1ae2f4
RICARDO ALVES SILVA <134711781+RicardoAlSi@users.noreply.github.com>
2026-04-09 10:31:03 -0300

    Atividade 3

---
+++ Atividade3/src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css"
+
+function App() {
+
+  return (
+    <div>
+      <main className="container">
+        <div className="item"><span className="n">1</span></div>
+        <div className="item"><span className="n">2</span></div>
+        <div className="item"><span className="n">3</span></div>
+        <div className="item"><span className="n">4</span></div>
+      </main>
+    </div>
+  )
+}
+
+export default App
